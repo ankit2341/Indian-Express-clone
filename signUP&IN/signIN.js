aa0cc9e06e70b48cd6dc595957c15dfe87d7d786
Yash Bhatter <yashbhatter.yb@gmail.com>
2022-07-21 16:22:18 +0530

    linked page(index-india-city-education-signin-signup)

--- signUP&IN/signIN.js
+++ signUP&IN/signIN.js
@@ -8,6 +8,7 @@ async function Login(){
     let response=await fetch(login_api_link,{
         method:'POST',
         body:login_data,
+        node:'no-cors',
         headers:
         {
             'Content-Type':'application/json',
@@ -15,9 +16,47 @@ async function Login(){
     });
     let data= await response.json();
     console.log('data:',data)
-    if(data.error==="false"){
-        alart("LogIn successfully")
-        window.location.href="./home.html";
+    if(data.error===false){
+        alert("LogIn successfully")
+        // document.querySelector("signIN").addEventListener("click", () => {
+        for(let i=0;i<=1;i++){
+            var count = 200;
+var defaults = {
+  origin: { y: 0.7 }
+};
+function fire(particleRatio, opts) {
+  confetti(Object.assign({}, defaults, opts, {
+    particleCount: Math.floor(count * particleRatio)
+  }));
+}
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+fire(0.2, {
+  spread: 60,
+});
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8
+});
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2
+});
+fire(0.1, {
+  spread: 200,
+  startVelocity: 45,
+});
+        }
+// });
+setTimeout(()=>{
+    window.location.href="index.html";
+},3000)
+        
     }else{
         alert("LogIn unsuccessfully");
         window.location.href="./home.html";
--- signUP&IN/signUP.js
+++ signUP&IN/signUP.js
@@ -23,4 +23,42 @@ async function Register(){
     let data= await response.json();
     console.log('data:',data)
     alert(data.message);
+    for(let i=0;i<=1;i++){
+        var count = 200;
+var defaults = {
+origin: { y: 0.7 }
+};
+function fire(particleRatio, opts) {
+confetti(Object.assign({}, defaults, opts, {
+particleCount: Math.floor(count * particleRatio)
+}));
+}
+fire(0.25, {
+spread: 26,
+startVelocity: 55,
+});
+fire(0.2, {
+spread: 60,
+});
+fire(0.35, {
+spread: 100,
+decay: 0.91,
+scalar: 0.8
+});
+fire(0.1, {
+spread: 120,
+startVelocity: 25,
+decay: 0.92,
+scalar: 1.2
+});
+fire(0.1, {
+spread: 200,
+startVelocity: 45,
+});
+    }
+// });
+setTimeout(()=>{
+window.location.href="index.html";
+},3000)
+
 }
