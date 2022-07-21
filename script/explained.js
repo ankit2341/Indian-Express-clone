1457e2a91f8265662413db1cd6aee2275c196f1f
Sachin Pal <s2638070@gmail.com>
2022-07-21 21:31:05 +0530

    political_pulse_add

--- script/explained.js
+++ script/explained.js
@@ -1,14 +1,40 @@
 let api_key= '480870360edd4e4b9114fe8724deed7d';
-let url='https://newsapi.org/v2/everything?q=explained&apiKey=480870360edd4e4b9114fe8724deed7d';
+let url=`https://newsapi.org/v2/everything?q=explained&apiKey=${api_key}`;
 
 let getData= async()=>{
 
     let res= await fetch(url);
     let data= await res.json();
     console.log(data);
-    append(data.articles)
+    append(data.articles);
+    append_img(data.articles);
 }
 getData()
+
+let i=0;
+
+let append_img=(data)=>{
+    let container=document.getElementById("big_img");
+    container.innerHTML=null;
+    data.forEach((el,i)=>{
+        if(i>data.length-2) {
+        let { urlToImage, title } = el;
+    let div=document.createElement("div");
+    let image = document.createElement("img");
+    image.src = urlToImage;
+    let name = document.createElement("h4");
+        name.innerText = title;
+        div.setAttribute("id", "content")
+        div.append(image,name);
+        div.addEventListener("click",()=>{
+            localStorage.setItem("clicked_item",JSON.stringify(el));
+            window.location.href="../landingpage/landing.html"
+        })
+        container.append(div)
+    }
+    })
+    }
+
 
 let append = (data)=>{
     let news= document.getElementById('news');
@@ -18,6 +44,7 @@ let append = (data)=>{
         let h3= document.createElement('h3')
         h3.innerText=`Explained: ${el.title}`;
         let content= document.createElement('div');
+        
         content.append(img,h3);
         content.addEventListener("click", ()=>{
             localStorage.setItem("clicked_item", JSON.stringify(el))
